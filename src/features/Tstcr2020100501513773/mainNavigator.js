c3c3a94be3e7bf50d450af0da227ab1637240d1d
Crowdbotics <team@crowdbotics.com>
2020-11-03 18:08:14 +0000

    add component tstcr2020100501

---
+++ src/features/Tstcr2020100501513773/mainNavigator.js
@@ -0,0 +1,36 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import {createDrawerNavigator} from 'react-navigation-drawer';
+
+import SplashScreen from "../features/SplashScreen";
+import SideMenu from './sideMenu';
+//@BlueprintImportInsertion
+import BlankScreen011733Navigator from '../features/BlankScreen011733/navigator';
+
+/**
+ * new navigators can be imported here
+ */
+
+const AppNavigator = {
+
+    //@BlueprintNavigationInsertion
+BlankScreen011733: { screen: BlankScreen011733Navigator },
+
+    /** new navigators can be added here */
+    SplashScreen: {
+      screen: SplashScreen
+    }
+};
+
+const DrawerAppNavigator = createDrawerNavigator(
+  {
+    ...AppNavigator,
+  },
+  {
+    contentComponent: SideMenu
+  },
+);
+
+const AppContainer = createAppContainer(DrawerAppNavigator);
+
+export default AppContainer;
